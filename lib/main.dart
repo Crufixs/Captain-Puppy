1f83d4bac0fc68941f40d5f0e2cb8a6e33035914
Crufixs <jcl.minano@gmail.com>
2021-11-20 23:45:59 +0800

    rawr

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        //ETITSSSSSS
+        // ETITSSSSSS
         // This is the theme of your application.
-        //
+        // HI KOBY RAWR ARWARAWRAWRAWAAAAAAAAAAAAAAAAAAAAAAAAAAAA
         // Try running your application with "flutter run". You'll see the
         // application has a blue toolbar. Then, without quitting the app, try
         // changing the primarySwatch below to Colors.green and then invoke
