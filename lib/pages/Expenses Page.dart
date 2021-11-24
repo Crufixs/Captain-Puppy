79a67b5c3d4044edc35880b3a333be5739dfb1f5
Crufixs <jcl.minano@gmail.com>
2021-11-25 00:25:17 +0800

    rawr

--- lib/pages/Expenses Page.dart
+++ lib/pages/Expenses Page.dart
@@ -1,4 +1,5 @@
 import 'package:fap/components/CustomNavBar.dart';
+import 'package:fap/components/ReusableComponent.dart';
 import 'package:fap/utilities/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
@@ -16,196 +17,213 @@ class _ExpensesPageState extends State<ExpensesPage> {
   int touchedIndex = -1;
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        children: <Widget>[
-          Expanded(
-            child: Center(
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Center(
               child: Text(
                 "EXPENSES",
                 style: TextTitle,
                 textAlign: TextAlign.center,
               ),
             ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Row(
-              //crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 8, right: 4),
-                    child: FlatButton(
-                      height: 55,
-                      color: current == ExpensesMode.summary
-                          ? firstColor
-                          : firstTransparentColor,
-                      onPressed: () {
-                        setState(() {
-                          current = ExpensesMode.summary;
-                        });
-                      },
-                      child: Text(
-                        "Summary",
-                        style: TextForButton,
+            Center(
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.75,
+                child: Row(
+                  //crossAxisAlignment: CrossAxisAlignment.stretch,
+                  //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 4),
+                        child: FlatButton(
+                          height: 55,
+                          color: current == ExpensesMode.summary
+                              ? firstColor
+                              : firstTransparentColor,
+                          onPressed: () {
+                            setState(() {
+                              current = ExpensesMode.summary;
+                            });
+                          },
+                          child: Text(
+                            "Summary",
+                            style: TextForButton,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 4, right: 8),
-                    child: FlatButton(
-                      height: 55,
-                      color: current == ExpensesMode.list
-                          ? firstColor
-                          : firstTransparentColor,
-                      onPressed: () {
-                        setState(() {
-                          current = ExpensesMode.list;
-                        });
-                      },
-                      child: Text(
-                        "List",
-                        style: TextForButton,
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 4),
+                        child: FlatButton(
+                          height: 55,
+                          color: current == ExpensesMode.list
+                              ? firstColor
+                              : firstTransparentColor,
+                          onPressed: () {
+                            setState(() {
+                              current = ExpensesMode.list;
+                            });
+                          },
+                          child: Text(
+                            "List",
+                            style: TextForButton,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
-          Expanded(
-            child: Row(
-              //crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                FlatButton(
-                  color: Colors.white,
-                  onPressed: () {},
-                  child: Text(
-                    "<",
-                    style: TextStyle(
-                      fontSize: 30,
+            Center(
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.75,
+                child: Row(
+                  //crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    FlatButton(
+                      color: Color(0x00000000),
+                      onPressed: () {},
+                      child: Text(
+                        "<",
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                Text("Octember", style: TextContent),
-                FlatButton(
-                  color: Colors.white,
-                  onPressed: () {},
-                  child: Text(
-                    ">",
-                    style: TextStyle(
-                      fontSize: 30,
+                    Text("Octember", style: TextContent),
+                    FlatButton(
+                      color: Color(0x00000000),
+                      onPressed: () {},
+                      child: Text(
+                        ">",
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
-          Expanded(
-            child: Text(
-              "Total Expenses: 0",
-              style: TextContent,
+            Center(
+              child: Text(
+                "Total Expenses: 0",
+                style: TextContent,
+              ),
             ),
-          ),
-          Expanded(
-            flex: 8,
-            child: current == ExpensesMode.summary
+            current == ExpensesMode.summary
                 ? getSummarySection()
                 : getListSection(),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 
-  Widget getListComponents(String picture, iconColor, breedName) {
-    return Row(
-      children: [
-        DecoratedBox(
-          child: Container(
-            padding: const EdgeInsets.all(8.00),
-            width: 70,
-            height: 70,
-            child: Image.asset(
-              'icons/dog_icons/$picture.png',
+  Widget getListComponents(String picture, iconColor, color, breedName) {
+    print("RAWR INSIDE");
+    return ReusableComponent(
+        insideComponents: Row(
+          children: [
+            DecoratedBox(
+              child: Container(
+                padding: const EdgeInsets.all(8.00),
+                width: 70,
+                height: 70,
+                child: Image.asset(
+                  'icons/dog_icons/$picture.png',
+                ),
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25.0),
+                color: iconColor,
+              ),
             ),
-          ),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(25.0),
-            color: iconColor,
-          ),
-        ),
-        SizedBox(
-          width: 20,
-        ),
-        Flexible(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                breedName,
-                style: TextStyle(
-                  fontSize: 23,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-              Flexible(
-                child: Text(
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.grey,
-                    fontWeight: FontWeight.normal,
+            SizedBox(
+              width: 20,
+            ),
+            Flexible(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    breedName,
+                    style: TextStyle(
+                      fontSize: 23,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
-                ),
+                  Flexible(
+                    child: Text(
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-      ],
-    );
+        iconColor: iconColor,
+        color: color,
+        breedName: "Rawr");
   }
 
   Widget getSummarySection() {
-    return PieChart(
-      PieChartData(
-        pieTouchData:
-            PieTouchData(touchCallback: (FlTouchEvent event, pieTouchResponse) {
-          setState(() {
-            if (!event.isInterestedForInteractions ||
-                pieTouchResponse == null ||
-                pieTouchResponse.touchedSection == null) {
-              touchedIndex = -1;
-              return;
-            }
-            touchedIndex = pieTouchResponse.touchedSection!.touchedSectionIndex;
-          });
-        }),
-        borderData: FlBorderData(
-          show: false,
+    return SizedBox(
+      height: MediaQuery.of(context).size.width * 0.90,
+      child: Container(
+        child: PieChart(
+          PieChartData(
+            pieTouchData: PieTouchData(
+                touchCallback: (FlTouchEvent event, pieTouchResponse) {
+              setState(() {
+                if (!event.isInterestedForInteractions ||
+                    pieTouchResponse == null ||
+                    pieTouchResponse.touchedSection == null) {
+                  touchedIndex = -1;
+                  return;
+                }
+                touchedIndex =
+                    pieTouchResponse.touchedSection!.touchedSectionIndex;
+              });
+            }),
+            borderData: FlBorderData(
+              show: false,
+            ),
+            sectionsSpace: 0,
+            centerSpaceRadius: 0,
+            sections: showingSections(),
+          ),
+          swapAnimationDuration: Duration(milliseconds: 150), // Optional
+          swapAnimationCurve: Curves.linear, // Optional
         ),
-        sectionsSpace: 0,
-        centerSpaceRadius: 40,
-        sections: showingSections(),
       ),
-      swapAnimationDuration: Duration(milliseconds: 150), // Optional
-      swapAnimationCurve: Curves.linear, // Optional
     );
   }
 
   Widget getListSection() {
-    final children = <Widget>[];
-    children.add(getListComponents("1dog", Colors.red, "Rawr"));
-    children.add(getListComponents("2dog", Colors.red, "Rawr"));
-    children.add(getListComponents("3dog", Colors.red, "Rawr"));
-    return ListView(
-      children: children,
+    print("rawr");
+    return SizedBox(
+      child: Column(
+        children: [
+          getListComponents("1dog", firstColor, firstTransparentColor, "Rawr"),
+          getListComponents("1dog", firstColor, firstTransparentColor, "Rawr"),
+          getListComponents("1dog", firstColor, firstTransparentColor, "Rawr"),
+          getListComponents("1dog", firstColor, firstTransparentColor, "Rawr")
+        ],
+      ),
     );
   }
 
@@ -213,7 +231,9 @@ class _ExpensesPageState extends State<ExpensesPage> {
     return List.generate(4, (i) {
       final isTouched = i == touchedIndex;
       final fontSize = isTouched ? 15.0 : 12.0;
-      final radius = isTouched ? 125.0 : 120.0;
+      final radius = isTouched
+          ? MediaQuery.of(context).size.width * 0.35
+          : MediaQuery.of(context).size.width * 0.325;
       switch (i) {
         case 0:
           return PieChartSectionData(
