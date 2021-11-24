0ed08eb4cd229ed0b7d8bd619b1265cd48f6687c
shrmncrystl <sharmainecrystel.arganosa.iics@ust.edu.ph>
2021-11-25 02:08:43 +0800

    .tit

---
+++ lib/components/User.dart
@@ -0,0 +1,22 @@
+//define all the fields
+class User {
+  final String username;
+  final String petimage;
+  final String petname;
+  final String breed;
+  final String gender;
+  final int age;
+  final double weight;
+  final String about;
+
+  const User({
+    required this.username,
+    required this.petimage,
+    required this.petname,
+    required this.breed,
+    required this.gender,
+    required this.age,
+    required this.weight,
+    required this.about,
+});
+}
