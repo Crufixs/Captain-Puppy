d5f2d3a34be8c4d8b356e97a25ce2c24743a2492
Koby <johnkobyreodica@gmail.com>
2021-11-25 15:50:01 +0800

    3 49PM

---
+++ lib/services/database_services.dart
@@ -0,0 +1,81 @@
+import 'package:fap/model/breed.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:flutter/widgets.dart';
+
+class DatabaseServices {
+
+  static final _dbName = 'captain_puppy_database.db';
+  static final _dbVersion = 1;
+  static var _database;
+
+  Future<Database> getDatabase () async{
+    if(_database!=null) {
+      print('HINDI');
+      return _database;
+    }
+    _database = await _initiateDatabase();
+    return _database;
+  }
+
+   _initiateDatabase() async{
+    WidgetsFlutterBinding.ensureInitialized();
+    return await openDatabase(
+        join(await getDatabasesPath(), _dbName),
+      onCreate: (db, version){
+          return db.execute(
+            'CREATE TABLE breed(breed_id INT PRIMARY KEY, name TEXT, life_span TEXT, temperament TEXT, weight TEXT, height TEXT, image_url TEXT)'
+          );
+      },
+      version: _dbVersion,
+    );
+  }
+
+  Future<void> insertBreed(Breed breed) async {
+    final db = await getDatabase();
+    await db.insert(
+      'breed',
+      breed.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<bool> isDatabaseExisting() async {
+    String path = join(await getDatabasesPath(), _dbName);
+    Future<bool> isDBExisting = databaseExists(path);
+
+    return isDBExisting;
+  }
+
+  Future<List<Breed>> generateBreedList() async{
+    final db = await getDatabase();
+    final List<Map<String, dynamic>> maps = await db.query('breed');
+
+    List<Breed> breedList = [];
+    for(int i=0; i<maps.length; i++){
+      Breed breed = Breed(
+          id: maps[i]['breed_id'],
+          name: maps[i]['name'],
+          lifeSpan: maps[i]['life_span'],
+          temperament: maps[i]['temperament'],
+          weight: maps[i]['weight'],
+          height: maps[i]['height'],
+          imageURL: maps[i]['image_url']
+      );
+      breedList.add(breed);
+    }
+
+    return breedList;
+    // return List.generate(maps.length, (i) {
+    //   return Breed(
+    //     id: maps[i]['id'],
+    //     name: maps[i]['name'],
+    //     lifeSpan: maps[i]['age'],
+    //     temperament: maps[i]['temperament'],
+    //     weight: maps[i]['weight'],
+    //     height: maps[i]['height'],
+    //     imageURL: maps[i]['image_url']
+    //   );
+    // });
+  }
+}
