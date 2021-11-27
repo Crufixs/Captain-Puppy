c1967722d9bc1663f2eb3dffac1e5971825e3065
andrei <janandreicarlos@gmail.com>
2021-11-27 20:38:56 +0800

    hi piii!! welcome page

--- lib/pages/welcome pages/Welcome page 1.dart
+++ lib/pages/welcome pages/Welcome page 1.dart
@@ -1,49 +1,63 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
-class WelcomePage1 extends StatelessWidget {
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
   List messages = [
-    "",
     "Lorem Ipsum is simply dummy text of the printing "
-    "and typesetting industry. Lorem Ipsum has been the industry's ",
+        "and typesetting industry. Lorem Ipsum has been the industry's ",
     "standard dummy text ever since the 1500s, when an unknown printer "
         "took a galley of type and scrambled it to make a type specimen book.",
     "and typesetting industry. Lorem Ipsum has been the industry's "
         "standard dummy text ever since the 1500s, when an unknown printer "
   ];
   int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          CarouselSlider(
-            options: CarouselOptions(height: MediaQuery.of(context).size.height),
-            items: [1,2,3].map((i) {
-              return Builder(
-                builder: (BuildContext context) {
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment:MainAxisAlignment.center,
+          children: [
+            CarouselSlider(
+              options: CarouselOptions(
+                  height: MediaQuery.of(context).size.height,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      _currentIndex = index;
+                    });
+                  }),
+              items: [0, 1, 2].map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
                           width: MediaQuery.of(context).size.width,
                           decoration: BoxDecoration(
-                              color: Colors.white,
+                            color: Colors.white,
                           ),
                           child: Image.asset("images/page$i.png"),
-                      ),
-                      Flexible(
+                        ),
+                        Flexible(
                           child: Text(messages[i]),
-
-                      )
-                    ],
-                  );
-                },
-              );
-            }).toList(),
-          ),
-          Expanded(
-            child: Row(
+                        )
+                      ],
+                    );
+                  },
+                );
+              }).toList(),
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: messages.map((urlOfItem) {
                 int index = messages.indexOf(urlOfItem);
@@ -59,12 +73,11 @@ class WelcomePage1 extends StatelessWidget {
                   ),
                 );
               }).toList(),
-            ),
-          )
-        ],
-
+            )
+          ],
+        ),
       ),
-
     );
   }
 }
+
