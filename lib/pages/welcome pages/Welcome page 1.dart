ec61e7b4bb54a0325652acbee8db7d3ac0acdfb0
Koby <johnkobyreodica@gmail.com>
2021-11-28 22:00:34 +0800

    WAKWAK

--- lib/pages/welcome pages/Welcome page 1.dart
+++ lib/pages/welcome pages/Welcome page 1.dart
@@ -1,6 +1,9 @@
+import 'package:fap/components/Button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+
+import '../Edit Profile.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -16,69 +19,92 @@ class _WelcomePageState extends State<WelcomePage> {
     "standard dummy text ever since the 1500s, when an unknown printer "
         "took a galley of type and scrambled it to make a type specimen book.",
     "and typesetting industry. Lorem Ipsum has been the industry's "
-        "standard dummy text ever since the 1500s, when an unknown printer "
+        "standard dummy text ever since the 1500s, when an unknown printer ",
+    ""
   ];
   int _currentIndex = 0;
 
+  generateTextOrButton(int index){
+    if(index != 3){
+      return Flexible(
+        child: Text(messages[index]),
+      );
+    } else {
+      return Button(
+        onClicked: () {
+          Navigator.push(
+            context, MaterialPageRoute(builder: (context) => EditProfile()),
+          );
+        },
+        hPadding: 10,
+        vPadding: 10,
+        text: 'Let\'s Get Started!',
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          // mainAxisAlignment:MainAxisAlignment.center,
-          children: [
-            CarouselSlider(
-              options: CarouselOptions(
-                  viewportFraction: 1,
-                  height: MediaQuery.of(context).size.height,
-                  onPageChanged: (index, reason) {
-                    setState(() {
-                      _currentIndex = index;
-                    });
-                  }),
-              items: [0, 1, 2].map((i) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          // width: MediaQuery.of(context).size.width,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            // mainAxisAlignment:MainAxisAlignment.center,
+            children: [
+              CarouselSlider(
+                options: CarouselOptions(
+                    viewportFraction: 1,
+                    // reverse: false,
+                    enableInfiniteScroll: false,
+                    height: MediaQuery.of(context).size.height,
+                    onPageChanged: (index, reason) {
+                      setState(() {
+                        _currentIndex = index;
+                      });
+                    }),
+                items: [0, 1, 2, 3].map((i) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            // width: MediaQuery.of(context).size.width,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                            ),
+                            child: Image.asset("images/page$i.png"),
                           ),
-                          child: Image.asset("images/page$i.png"),
-                        ),
-                        Flexible(
-                          child: Text(messages[i]),
-                        ),
-                        Expanded(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: messages.map((urlOfItem) {
-                              int index = messages.indexOf(urlOfItem);
-                              return Container(
-                                width: 10.0,
-                                height: 10.0,
-                                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: _currentIndex == index
-                                      ? Color.fromRGBO(0, 0, 0, 0.8)
-                                      : Color.fromRGBO(0, 0, 0, 0.3),
-                                ),
-                              );
-                            }).toList(),
-                          ),
-                        )
-                      ],
-                    );
-                  },
-                );
-              }).toList(),
-            ),
-          ],
+                          generateTextOrButton(i),
+                          Expanded(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: messages.map((urlOfItem) {
+                                int index = messages.indexOf(urlOfItem);
+                                return Container(
+                                  width: 10.0,
+                                  height: 10.0,
+                                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: _currentIndex == index
+                                        ? Color.fromRGBO(0, 0, 0, 0.8)
+                                        : Color.fromRGBO(0, 0, 0, 0.3),
+                                  ),
+                                );
+                              }).toList(),
+                            ),
+                          )
+                        ],
+                      );
+                    },
+                  );
+                }).toList(),
+              ),
+            ],
+          ),
         ),
       ),
     );
